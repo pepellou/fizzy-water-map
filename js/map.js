de6c8c93b39bf835ccbbeb67baf5547dd1e35a2e
Pepe Doval <pepellou@gmail.com>
2019-03-10 14:16:55 -0500

    Fix ohjay_ nickname

--- js/map.js
+++ js/map.js
@@ -8,7 +8,7 @@ var Drinker = function(name, gps) {
 var squad = [
     new Drinker('pepellou', [42.85, -8.63]),
     new Drinker('Whiskey1983', [51.51, -0.12]),
-    new Drinker('ojb', [53.42, -1.35]),
+    new Drinker('ohjay_', [53.42, -1.35]),
     new Drinker('JohnJohn134', [57.05, 9.92]),
     new Drinker('JoannaTries', [-93.17, 44.804])
 ];
