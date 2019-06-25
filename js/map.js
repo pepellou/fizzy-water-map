e6ca2d8f9c2ce1cde7e1323083939b135b1af1c6
Pepe Doval <pepellou@gmail.com>
2019-06-25 05:30:33 -0500

    Add wch355

--- js/map.js
+++ js/map.js
@@ -27,6 +27,7 @@ var squad = [
     new Drinker('sefanstien', [40.21,28.98]),
     new Drinker('gunfunkel', [47.88,12.41]),
     new Drinker('Morphy73', [43.12,5.92]),
+    new Drinker('wch355', [-12.97,-38.5]),
 ];
 
 var map = L
